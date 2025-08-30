9c54bf3c5c8ae199e5568c479cf2e3868a5b711a
DeepjanS395373 <227720068+DeepjanS395373@users.noreply.github.com>
2025-08-30 13:24:26 +0930

    fix: typo error fixe  utf.

--- clean_function.py
+++ clean_function.py
@@ -1,8 +1,8 @@
-"""We write all function here but no actual input or output"""
-
+"""We write all function here but no actual input or outpu. Flake8 and pylint is used to standarlize code with docstring , whitepasces 
+and comments"""
 import pandas as pd
 
-def read_data(path ):
-    """Read csv data with encoding"""
-    
-    return pd.read_cs
+
+def read_data(path):
+    """Read csv data with encoding"""  
+    return pd.read_csv(path, encoding="utf-8-sig")
