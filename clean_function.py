f92bc3b11510920d1ea48543667d54a5ad04d7dd
DeepjanS395373 <227720068+DeepjanS395373@users.noreply.github.com>
2025-09-01 14:52:32 +0930

    Add: Function to view summary of dataconversion.

--- clean_function.py
+++ clean_function.py
@@ -9,7 +9,7 @@ def read_data(path):
     return pd.read_csv(path, encoding="utf-8-sig")
 
 
-def check_few_values(df, name):
+def check_few_values(df, name="Dataset"):
     print(f"\nChecking value for {name}:")
     print(df.head())
 
@@ -21,10 +21,10 @@ def missing_values_and_datatype_find(df, col):
           f"({missing/len(df):.1%})")
 
 
-def detect_and_convert_datetime(df, name):
-    """This checks the object datatype and converts it to date and 
+def detect_and_convert_datetime(df, name="Dataset"):
+    """This checks the object datatype and converts it to date and
     time if possible."""
-    # Keep track of conversion results    
+    # Keep track of conversion results.
     result = {}
     for col in df.columns:
         # Only check for object datatypes.
@@ -44,3 +44,36 @@ def detect_and_convert_datetime(df, name):
     for col, status in result.items():
         print(f"{col}:{status}")
     return df
+
+
+def detect_and_convert_number(df, name="Dataset"):
+    """This checks the object datatype and converts it to number
+    if possible."""
+    # Keep track of conversion results.
+    result = {}
+    for col in df.columns:
+        # Only check for object datatypes.
+        if df[col].dtype == "object":
+            try:
+                df[col] = pd.to_numeric(df[col], errors="raise")
+                # print(f"Converted column '{col}' to numberic datatype")
+                result[col] = "Converted to numeric datatype."
+            except Exception:
+                # If conversion fails, leave it as object.
+                # and record the result.
+                result[col] = "Not a numeric column."
+
+    # Print conversion result.
+    print(f"\nNumeric conversion result for {name}")
+    for col, status in result.items():
+        print(f"{col}:{status}")
+    return df
+
+
+def datatype_conversion_summary(df, name="Dataset"):
+    """Displays the summary of column types after conversionS"""
+    print(f"\n Summary of {name}:")
+    # Counts total of each datatype.
+    print(df.dtypes.value_counts())
+    print("\nDetailed Viwe:")
+    print(df.dtypes)
