78be60b3493f7c6b9030a4eb21db0e085585f048
DeepjanS395373 <227720068+DeepjanS395373@users.noreply.github.com>
2025-09-01 14:17:15 +0930

    Fix: Error displayed in dataconversion fixed for datetime.

--- clean_function.py
+++ clean_function.py
@@ -19,19 +19,28 @@ def missing_values_and_datatype_find(df, col):
     missing = df[col].isna().sum()
     print(f"Column: {col},", "dtype:", df[col].dtype, "missing:", missing,
           f"({missing/len(df):.1%})")
-    
+
 
 def detect_and_convert_datetime(df):
     """This checks the object datatype and converts it to date and 
     time if possible."""
-    
+    # Keep track of conversion results    
+    result = {}
     for col in df.columns:
         # Only check for object datatypes.
         if df[col].dtype == "object":
             try:
-                df[col] = pd.to_datetime(df[col], errors="raise")
-                print(f"Converted column '{col}' to datetime")
+                df[col] = pd.to_datetime(df[col], dayfirst=True,
+                                         errors="coerce")
+                # print(f"Converted column '{col}' to datetime")
+                result[col] = "Converted to datetime"
             except Exception:
                 # If conversion fails, leave it as object.
-                pass
+                # and record the result.
+                result[col] = "Not a datetime column."
+
+    # Print conversion result.
+    print("\nDatetime conversion result.")
+    for col, status in result.items():
+        print(f"{col}:{status}")
     return df
